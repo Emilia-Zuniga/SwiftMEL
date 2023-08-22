3804243e96715b135e38394685d160f1fe8bbadd
Emilia Zuñiga Losada <emiliazlosada14@outlook.com>
2023-08-22 12:35:13 -0600

    Initial Commit

---
+++ SwiftMEL/SwiftMELApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMELApp.swift
+//  SwiftMEL
+//
+//  Created by Emilia Zuñiga Losada on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMELApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
